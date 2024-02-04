e53cac812bfba08674c489eecab7ea2a7c206db6
AF332 <134966071+AF332@users.noreply.github.com>
2024-02-04 15:22:13 +0000

    Add files via upload

--- Preprocessing.py
+++ Preprocessing.py
@@ -7,17 +7,16 @@ import tensorflow as tf
 # Need to make a environment on one of the computer in G2 for this code.
 
 # Create a nifti data loader
-def load_nifti_files(directory_image, directory_mask):  # Defines a function named 'load_nifti_files' whuich takes one argument, 'directory', representing the path to the folder containgthe NIFTI files.
-    mri_files_image = sorted(glob.glob(os.path.join(directory_image, "*.nii"))) # The file paths are sorted in a list alphabetically. All files in the directory that match the pattern then constructs a file path patten by joininh the directory path with the filename pattern.
-    data_image = [] # Initialising an empty list for the image
+def load_nifti_files(directory_image, directory_mask):  # Defines a function named 'load_nifti_files' which takes 2 different directory as the arguement, representing the path to the folder containing the images and masks NIFTI files.
+    mri_files_image = sorted(glob.glob(os.path.join(directory_image, "*.nii"))) # All files in the directory with the ending .nii are returned as a list of file paths and then sorted lexicographically, which is based on the ASCII values of the characters in the filenames.
     image_info = [] # List to store the info of each image
+    data_image = [] # List to store all the images  
 
     for file in mri_files_image: # For every file in the folder
         image = nib.load(file) # The file is loaded
         image_data = image.get_fdata() # The data from the file is extracted
         num_slices = image_data.shape[-1] # The number of slices are extracted
         image_info.append({'filename': file, 'shape': image_data.shape, 'num_slices': num_slices})
-        print(image_info)
 
         for i in range(num_slices): 
             slice = image_data[:, :, i]
@@ -32,18 +31,13 @@ def load_nifti_files(directory_image, directory_mask):  # Defines a function nam
         mask_data = mask.get_fdata()
         num_slices = mask_data.shape[-1]
         mask_info.append({'filename': file, 'shape': mask_data.shape, 'num_slices': num_slices})
-        print(mask_info)
+        print(mask_info[0])
 
         for i in range(num_slices):
             slice = mask_data[:, :, i]
             data_mask.append(slice)
 
-# Check if each image has a corresponding mask with the same shape
-    for img_info, mask_info in zip(image_info, mask_info):
-        if img_info['shape'] != mask_info['shape']:
-            print(f"Mismatch found: Image file {img_info['filename']} and Mask file {mask_info['filename']} have different shapes: {img_info['shape']} vs {mask_info['shape']}")
-
-    return np.array(data_image), np.array(data_mask)
+    return np.array(data_image), np.array(data_mask), np.array(image_info), np.array(mask_info)
 # All the elements present in the list don't have the same shape or length.
 # Will have to resize, crop or padd the elements to be consistent but first need to find which one needs this function applied to.
 
@@ -56,7 +50,11 @@ directory_mask = r"F:\Masks\T2"
 # The last image and mask does not match. The last image is (320,320) and the last mask is (384, 384).
 
 # Load Data
-data_image, data_mask = load_nifti_files(directory_image, directory_mask)
+data_image, data_mask, image_info, mask_info = load_nifti_files(directory_image, directory_mask)
+
+print(image_info.shape)
+print(mask_info.shape)
+# why is the mask when turned into an array doesn't show?
 
 print("Data Image shape:", data_image.shape)
 print("Data Image dtype:", data_image.dtype)
